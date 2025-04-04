ea332be5fe45166fc26a4f6add7ae550ddc78ea4
okhaled11 <mido.loda2002@gmail.com>
2025-04-04 23:04:19 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+//  LIGHT AND DARk MODE
+
+const themeBtn = document.querySelector("[data-theme-btn]");
+const htmlElement = document.documentElement;
+
+themeBtn.addEventListener("click", () => {
+    const currentTheme = htmlElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    htmlElement.setAttribute("data-theme", newTheme);
+});
+
+
+// TAB
+
+const tabBtns = document.querySelectorAll("[data-tab-btn]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+
+let lastActiveTab = document.querySelector("[data-tab-content].active") || tabContents[0];
+let lastActiveTabBtn = document.querySelector("[data-tab-btn].active") || tabBtns[0];
+
+tabBtns.forEach(btn => {
+    btn.addEventListener("click", function () {
+        if (lastActiveTab) lastActiveTab.classList.remove("active");
+        if (lastActiveTabBtn) lastActiveTabBtn.classList.remove("active");
+
+        const newTabContent = document.querySelector(`[data-tab-content="${btn.dataset.tabBtn}"]`);
+
+        if (newTabContent) {
+            newTabContent.classList.add("active");
+            btn.classList.add("active");
+
+            lastActiveTab = newTabContent;
+            lastActiveTabBtn = btn;
+        }
+    });
+});
+
+
+
+
